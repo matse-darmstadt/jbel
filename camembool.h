88c1136014ba34eb73f9a71a1644a81d7ee31859
jembel <am-heh@arcor.de>
2015-08-25 11:10:30 +0200

    some more cheesy methods

--- camembool.h
+++ camembool.h
@@ -1,16 +1,23 @@
-class camembool
+class Camembool
 {
 public:
 	// Constructor
 	// will have to make sure the sides are cheese only
-	camembool();
-	// Destructor
-	~camembool();
+	Camembool();
+	// Destructor (mouse breathing air and eating cheese?)
+	~Camembool();
 	// the cheese cube: a 3d bool array
 	// nxnxn (a cube, duh!)
-	bool cube [][][];
+	// or maybe leave dimensions optional in case of cheese slices? (i.e. 2d)
+	bool cube[][][];
 private:
 	// returns an array of 3 chars which represent coordinates
 	// we're not gonna use cubes larger than 256, are we?
-	char* GetEntryPoint();
+	char* GetEntryPoint(cube* cheese);
+	// returns the number of steps taken until
+	int CountPathSteps(int counter);
+	// bad pun on bouillon
+	void PourBoollon(char* entrypoint);
+	
+	
 };
