1e6afad31f8e30fbc6d595e8a9e1c2c35626f765
jembel <am-heh@arcor.de>
2015-08-25 11:29:21 +0200

    pushing cheese

--- camembool.h
+++ camembool.h
@@ -15,9 +15,13 @@ private:
 	// we're not gonna use cubes larger than 256, are we?
 	char* GetEntryPoint(cube* cheese);
 	// returns the number of steps taken until
+	// can be used as recursive method for recursivenesses sake
 	int CountPathSteps(int counter);
 	// bad pun on bouillon
-	void PourBoollon(char* entrypoint);
-	
-	
+	// initiates the search of a path through the cube
+	void PourBoollon(char* coord);
+	// determines what the cube below the coordinates is made off
+	bool CheckPointBelow(char* coord);
+	// returns the 3x3 cube around the coordinates
+	cube* GetNeighbours(char* coord);
 };
