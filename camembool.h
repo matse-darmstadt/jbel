0d6ad843d7f33228c2233df83a8ac2fac056fe6c
jembel <am-heh@arcor.de>
2015-08-25 15:25:51 +0200

    https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcR9TEZa34GkCtG0HysKEsf9yWnjhEBPe5o9THL9D-LImEgIgmVfcA

--- camembool.h
+++ camembool.h
@@ -3,27 +3,35 @@ class Camembool
 public:
 	// Constructor
 	// will have to make sure the sides are cheese only
-	Camembool();
+	Camembool(char dim);
 	// Destructor (mouse breathing air and eating cheese?)
 	~Camembool();
+	// fills the cube with cheese and air in a fixed pattern
+	void RipenFixedCube();
+	// fills the cube with cheese and air in a random pattern
+	// probably not going to be used
+	void RipenRandomCube();
+	// lets the user decide the pattern of the cheese cube
+	// implementation/arguments unclear
+	// probably not going to be used as well
+	void RipenCustomCube();
+
+
+private:
 	// the cheese cube: a 3d bool array
 	// nxnxn (a cube, duh!)
 	// or maybe leave dimensions optional in case of cheese slices? (i.e. 2d)
 	bool cube [][][];
 	// the dimension of the cube (i.e. length of the edges)
-	char dimensions;
-private:
-	// fills the cube with cheese and air in a fixed pattern 
-	cube* RipenFixedCube();
-	// fills the cube with cheese and air in a random pattern
-	// probably not going to be used
-	cube* RipenRandomCube();
-	// lets the user decide the pattern of the cheese cube
-	// implementation/arguments unclear
-	// probably not going to be used as well
-	cube* RipenCustomCube();
-	// returns an array of 3 chars which represent segments
 	// we're not gonna use cubes larger than 256, are we?
+	char dimension;
+	// the current path through the cube
+	char* path;
+	//
+	const char FIXED_DIM = 4;
+
+
+	// returns an array of 3 chars which represent segments
 	char* GetEntryPoint(cube* cheese);
 	// returns the number of steps taken until
 	// can be used recursively for recursiveness' sake
@@ -36,7 +44,7 @@ private:
 	// returns the 3x3 cube around the coordinates
 	cube* GetNeighbours(char* coord);
 	// an array of coordinates (char[x][3]) that represents the path through the cube
-	char* [] WalkPath();
+	char* WalkPath();
 	// verifies if the intended step is valid
 	bool VerifyStep(char* curr, char* next);
 	// takes a look at the neighbours at the coordinates to make sure we're not leaving the cube's boundaries
