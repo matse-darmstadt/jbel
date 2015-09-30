5737bc8b80d7145aa793cacdc5275b2794658419
matse-bkra <freebird83@gmx.de>
2015-09-30 17:09:52 +0200

    more tests

--- camemboolTest.cpp
+++ camemboolTest.cpp
@@ -8,6 +8,7 @@
 #include "camembool.h"
 #include <iostream>
 #include <functional>
+#include <stdlib.h>
 
 using namespace std;
 typedef unsigned char uchar;
@@ -38,11 +39,12 @@ void assertEqual(bool testValue, bool expectedValue, string testName) {
 
 
 void printTestSummary() {
-	cout << "Number of tests: " << testCounter << "\t"
+	cout <<"\n" << "Number of tests: " << testCounter << "\t"
 		<< "SUCCESSFUL tests: " << (testCounter - testFailCounter) << "\t"
 		<< "FAILED tests: " << testFailCounter << endl;
 }
 
+// TODO pull type of Camembool out of test methods and call those with the type to test
 class CamemboolTest {
 
 
@@ -94,6 +96,24 @@ public:
 		}
 		return Camembool(cube, size);
 	}
+
+	Camembool getRandomCube(uchar size) {
+		bool*** cube = new bool**[size];
+		for (uchar x = 0; x < size; x++) {
+			cube[x] = new bool*[size];
+			for (uchar y = 0; y < size; y++) {
+				cube[x][y] = new bool[size];
+				for (uchar z = 0; z < size; z++) {
+					int r = rand();
+					if (r % 2 == 0)
+						cube[x][y][z] = AIR;
+					else
+						cube[x][y][z] = CHEESE;
+				}
+			}
+		}
+		return Camembool(cube, size);
+	}
 	Camembool createCamembool(unsigned char size,
 								function<bool(uchar x, uchar y, uchar z)> cheeseGenerator) {
 
@@ -116,9 +136,31 @@ public:
 	void testCheckBoundary() {
 
 		Camembool c = getAllCheese(5);
+		int numberOfValidSegments = 0;
+		uchar segment[3];
+		for (uchar x = -1 ; x <= c.size ; x++) {
+			segment[0] = x;
+			segment[1] = 0;
+			segment[2] = 0;
+			if (c.CheckBoundary(segment))
+				numberOfValidSegments++;
+		}
 
-		uchar segment[] = { 2, 2, 3 };
-		cout << c.CheckBoundary(segment) << endl;
+		assertTrue(numberOfValidSegments == 5, "Test:checkBoundary::allCheeseCube");
+
+		c = getAllAir(5);
+		numberOfValidSegments = 0;
+		// 0xFF
+		segment[0] = -1;
+		segment[1] = -1;
+		segment[2] = -1;
+		assertTrue(c.CheckBoundary(segment) == false, "Test::checkBoundary::allAir::invalidPoint");
+
+		segment[0] = 3;
+		segment[1] = 2;
+		segment[2] = 1;
+		assertTrue(c.CheckBoundary(segment) == true, "Test::checkBoundary::allAir::validPoint");
+
 	}
 
 	void testGetNeighbours() {
@@ -131,34 +173,80 @@ public:
 		c.GetNeighbours(segment, neighbourCoords, numberOfNeighbours);
 		assertTrue(numberOfNeighbours == 0, "Test::GetNeighbours::allCheeseCube");
 
-		segment = {2,0,2};
+		c = getAllAir(5);
+		segment[0] = 2;
+		segment[1] = 2;
+		segment[2] = 2;
+		c.GetNeighbours(segment, neighbourCoords, numberOfNeighbours);
+		assertTrue(numberOfNeighbours == 6, "Test::GetNeighbours::allAirCube");
+
+		segment[0] = 2;
+		segment[1] = 0;
+		segment[2] = 2;
 		c = getCubeWithHoleInMiddle(5);
 		c.GetNeighbours(segment, neighbourCoords, numberOfNeighbours);
-		assertTrue(numberOfNeighbours == 1, "Test::GetNeighbours::CubeWithHoleInMiddle");
+		assertTrue(numberOfNeighbours == 1, "Test::GetNeighbours::CubeWithHoleInMiddle:numberOfNeighbours");
+		assertTrue(neighbourCoords[0] == 2 && neighbourCoords[1] == 1 && neighbourCoords[2] == 2,
+					"Test::GetNeighbours::CubeWithHoleInMiddle:neighbourCoords");
 	}
 
 	void testWalkPath() {
 		Camembool c = getAllCheese(5);
+		assertTrue(!c.WalkPath(), "Test::walkPath::allCheeseCube");
 
+		c = getAllAir(5);
+		assertTrue(c.WalkPath(), "Test::walkPath::allAirCube");
+
+		c = getCubeWithHoleInMiddle(5);
+		assertTrue(c.WalkPath(), "Test::walkPath::CubeWithHileInMiddle");
 	}
 
 	void testGetEntryPoint() {
 
 		Camembool c = getAllCheese(5);
-		uchar* result = c.GetEntryPoint();
-		assertTrue(result == nullptr, "Test::GetEntryPoint::allCheeseCube::null");
+		assertTrue(c.GetEntryPoint() == nullptr, "Test::GetEntryPoint::allCheeseCube::null");
+
+		c = getAllAir(5);
+		assertTrue(c.GetEntryPoint() != nullptr, "Test::GetEntryPoint::allAirCube");
 
 		c = getCubeWithHoleInMiddle(5);
-		assertTrue(c.cube[2][0][2] == AIR, "Test::GetEntryPoint::middleHoleCube::singleValidPoint");
+		uchar* result = c.GetEntryPoint();
+		uchar expectedCoords[] = {2,0,2};
+		bool match = true;
+		for (uchar i = 0 ; i < 3 ; i++) {
+			if (result[i] != expectedCoords[i])
+				match = false;
+		}
+		assertTrue(match, "Test::GetEntryPoint::CubeWithHoleInMiddle::singleValidPoint");
 	}
 
 	void testCountPathSteps() {
 		Camembool c = getAllCheese(5);
+		unsigned int numberOfSteps = c.CountPathSteps();
+		assertTrue(numberOfSteps == 0, "Test::CountPathSteps::allCheeseCube");
 
+		c = getAllAir(5);
+		numberOfSteps = c.CountPathSteps();
+		assertTrue(numberOfSteps == 5, "Test::CountPathSteps::allCheeseCube");
+
+		c = getCubeWithHoleInMiddle(5);
+		numberOfSteps = c.CountPathSteps();
+		assertTrue(numberOfSteps == 5, "Test::CountPathSteps::allCheeseCube");
 	}
 
 	void testCheckSegmentBelow() {
+
 		Camembool c = getAllCheese(5);
+		uchar segment[] = {0,0,0};
+		bool belowSegmentType = c.CheckSegmentBelow(segment);
+		assertTrue(belowSegmentType == CHEESE, "Test::CheckSegmentBelow::allCheeseCube");
+
+		c = getCubeWithHoleInMiddle(5);
+		segment[0] = 2;
+		segment[0] = 0;
+		segment[0] = 2;
+		belowSegmentType = c.CheckSegmentBelow(segment);
+		assertTrue(belowSegmentType == AIR, "Test::CheckSegmentBelow::allCheeseCube");
 
 	}
 
@@ -174,10 +262,15 @@ int main() {
 	CamemboolTest test;
 
 	test.testCheckBoundary();
-
 	test.testGetNeighbours();
-
+	test.testWalkPath();
 	test.testGetEntryPoint();
+	test.testCountPathSteps();
+	test.testCheckSegmentBelow();
+	test.testVerifyStep();
+
+
+
 
 	printTestSummary();
 }
