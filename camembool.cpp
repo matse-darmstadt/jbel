5885a95de3dc1068230ae153cc71893949f83c86
jembel <j.belmokhtar@mgsoftech.com>
2015-09-30 15:22:41 +0200

    stuff

--- camembool.cpp
+++ camembool.cpp
@@ -6,20 +6,59 @@
  */
 #include "camembool.h"
 
-	Camembool::Camembool(unsigned char size){}
-	Camembool::Camembool(bool*** cube, unsigned char size){}
-	Camembool::~Camembool(){}
+Camembool::Camembool(unsigned char size): cube(nullptr), size(size), path(nullptr) {
+}
+Camembool::Camembool(bool*** cube, unsigned char size): cube(cube), size(size), path(nullptr) {
+}
+Camembool::~Camembool() {
+}
 
-	void Camembool::RipenFixedCube(){}
-	void Camembool::RipenRandomCube(){}
-	void Camembool::RipenCustomCube(bool*** cube){}
-	void Camembool::PourBoollon(char* coord){}
-	char* Camembool::GetEntryPoint(){char* segment = new char[3]; return segment;}
-	int Camembool::CountPathSteps(){return 0;}
-	bool Camembool::CheckSegmentBelow(char* coord){return true;}
-	void Camembool::GetNeighbours(char* coord, unsigned char*& neighbourCoords, unsigned char& numberOfValidNeighbours){}
-	bool Camembool::WalkPath(){return true;}
-	bool Camembool::VerifyStep(char* curr, char* next){return true;}
-	bool Camembool::CheckBoundary(char* coord){return true;}
+void Camembool::RipenFixedCube() {
+}
+void Camembool::RipenRandomCube() {
+}
+void Camembool::RipenCustomCube(bool*** cube) {
+}
+void Camembool::PourBoollon(char* coord) {
+}
 
+
+unsigned char* Camembool::GetEntryPoint() {
+
+	unsigned char x, z;
+	x = 0;
+	z = 0;
+	while (x != size || z != size)
+	{
+		if (cube[x++][0][z++] == false)
+		{
+			unsigned char* segment = new unsigned char[3];
+			segment[0] = x;
+			segment[1] = 0;
+			segment[2] = z;
+			return segment;
+		}
+	}
+	return nullptr;
+}
+
+
+unsigned int Camembool::CountPathSteps() {
+	return 0;
+}
+bool Camembool::CheckSegmentBelow(unsigned char* coord) {
+	return true;
+}
+void Camembool::GetNeighbours(unsigned char* coord, unsigned char*& neighbourCoords,
+		unsigned char& numberOfValidNeighbours) {
+}
+bool Camembool::WalkPath() {
+	return true;
+}
+bool Camembool::VerifyStep(unsigned char* curr, unsigned char* next) {
+	return true;
+}
+bool Camembool::CheckBoundary(unsigned char* coord) {
+	return true;
+}
 
--- camembool.h
+++ camembool.h
@@ -99,7 +99,7 @@ private:
 	/*
 	 * The cubes edge length (number of fields per array dimension)
 	 */
-	char size;
+	unsigned char size;
 	
 	/*
 	 * a char pointer to a n x 3 array
@@ -108,15 +108,8 @@ private:
 	 * stores the current path through the cube
 	 * 
 	 */
-	char* path;
+	unsigned char* path;
 	
-	/*
-	 * just a constant array size for the fixed cube
-	 * might as well be in there ...probably better anyway
-	 * 
-	 */
-	const char FIXED_SIZE = 4;
-
 	/*
 	 * name: GetEntryPoint
 	 * looks for the NEXT air segment in the top layer (z == 0)
@@ -128,7 +121,7 @@ private:
 	 * @return: pointer to a char array (size 3); coordinates, obviously
 	 * 
 	 */
-	char* GetEntryPoint();
+	unsigned char* GetEntryPoint();
 	
 	/*
 	 * name: CountPathSteps
@@ -140,7 +133,7 @@ private:
 	 * @return: int path.length
 	 * 
 	 */
-	int CountPathSteps();
+	unsigned int CountPathSteps();
 	
 	/*
 	 * name: CheckSegmentBelow
@@ -153,7 +146,7 @@ private:
 	 * only two choices here, true for cheese and false for air
 	 * 
 	 */
-	bool CheckSegmentBelow(char* coord);
+	bool CheckSegmentBelow(unsigned char* coord);
 	
 	/*
 	 * name: GetNeighbours
@@ -171,7 +164,7 @@ private:
 	 * pointer to the bool array
 	 * 
 	 */
-	void GetNeighbours(char* coord, unsigned char*& neighbourCoords, unsigned char& numberOfValidNeighbours);
+	void GetNeighbours(unsigned char* coord, unsigned char*& neighbourCoords, unsigned char& numberOfValidNeighbours);
 	
 	/*
 	 * name: WalkPath
@@ -196,7 +189,7 @@ private:
 	 * @return: bool validStep
 	 * true if ok, false if not
 	 */
-	bool VerifyStep(char* curr, char* next);
+	bool VerifyStep(unsigned char* curr, unsigned char* next);
 	
 	/*
 	 * name: CheckBoundary
@@ -212,5 +205,5 @@ private:
 	 * NOTE: might make more sense to know something about where the error came from (i.e. to the right, the bottom etc.)
 	 * 		 again depends on the final implementation, sorry
 	 */
-	bool CheckBoundary(char* coord);
+	bool CheckBoundary(unsigned char* coord);
 };
