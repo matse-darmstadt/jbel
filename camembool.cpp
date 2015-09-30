6368f664703e1f2697a0a6f3abd1f3eb9b4edd10
matse-bkra <freebird83@gmx.de>
2015-09-30 13:46:35 +0200

    api changed again :)

--- camembool.cpp
+++ camembool.cpp
@@ -14,12 +14,12 @@
 	void Camembool::RipenRandomCube(){}
 	void Camembool::RipenCustomCube(bool*** cube){}
 	void Camembool::PourBoollon(char* coord){}
-	char* Camembool::GetEntryPoint(){}
-	int Camembool::CountPathSteps(){}
-	bool Camembool::CheckSegmentBelow(char* coord){}
-	bool* Camembool::GetNeighbours(char* coord){}
-	bool Camembool::WalkPath(){}
-	bool Camembool::VerifyStep(char* curr, char* next){}
-	bool Camembool::CheckBoundary(char* coord){}
+	char* Camembool::GetEntryPoint(){char* segment = new char[3]; return segment;}
+	int Camembool::CountPathSteps(){return 0;}
+	bool Camembool::CheckSegmentBelow(char* coord){return true;}
+	void Camembool::GetNeighbours(char* coord, unsigned char*& neighbourCoords, unsigned char& numberOfValidNeighbours){}
+	bool Camembool::WalkPath(){return true;}
+	bool Camembool::VerifyStep(char* curr, char* next){return true;}
+	bool Camembool::CheckBoundary(char* coord){return true;}
 
 
